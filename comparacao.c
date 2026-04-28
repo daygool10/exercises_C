c410c5c0e69031c3926b70eaeec088fcf98b0f81
daygool🏁 <daiane.rocha2017@gmail.com>
2026-04-28 17:58:47 -0300

    comparação entre strings (strcmp)

---
+++ comparacao.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+char a[] = "Giorgian", b[] = "Giorgian";
+
+
+if(strcmp(a,b)== 0){
+    printf("Strings iguais");
+}else {
+    printf("Strings diferentes");
+}
+
+
+
+return 0;
+
+}
+
